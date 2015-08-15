fb951fee060121de314b2dc84b63ae75e81e8cfc
Martin Kim Dung-Pham <kim@elbedev.com>
2015-08-15 14:06:24 +0200

    Initial Commit

---
+++ MailWatch/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  MailWatch
+//
+//  Created by Martin Kim Dung-Pham on 15.08.15.
+//  Copyright © 2015 Martin Kim Dung-Pham. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
