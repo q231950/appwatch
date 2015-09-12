34e3ad136a1ddc3d6f5575269e510e6daf29affe
Martin Kim Dung-Pham <kim@elbedev.com>
2015-09-12 13:17:10 +0200

    Adds test for persisting application observers within application observer controller

--- AppWatchLoginItem/AppWatchLoginItemTests/ApplicationObserverControllerTest.swift
+++ AppWatchLoginItem/AppWatchLoginItemTests/ApplicationObserverControllerTest.swift
@@ -9,11 +9,32 @@
 import XCTest
 @testable import AppWatchLoginItem
 
-class ApplicationObserverControllerTest: XCTestCase {
+class ApplicationObserverMock : ApplicationObserver {
+    var didCallPersist = false
+    
+    override func persistObservations() {
+        didCallPersist = true
+    }
+}
 
+class ApplicationObserverControllerTest: XCTestCase {
+    
     func testAddsObserverPerIdentifier() {
         let controllerUnderTest = ApplicationObserverController(identifiers: ["a", "b", "c"])
         XCTAssertEqual(controllerUnderTest.applicationObservers.count, 3)
+    }
+    
+    func testPersistsObservations() {
+        let controllerUnderTest = ApplicationObserverController(identifiers: ["a", "b", "c"])
+        let mock1 = ApplicationObserverMock(applicationBundleIdentifier: "", timeBox: TimeBox(), timeWriter: FileTimeWriter())
+        let mock2 = ApplicationObserverMock(applicationBundleIdentifier: "", timeBox: TimeBox(), timeWriter: FileTimeWriter())
+        
+        controllerUnderTest.applicationObservers = [mock1, mock2]
+        
+        controllerUnderTest.persistObservations()
+        
+        XCTAssertTrue(mock1.didCallPersist)
+        XCTAssertTrue(mock2.didCallPersist)
     }
 
 }
